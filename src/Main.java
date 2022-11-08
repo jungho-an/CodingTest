e64e68ee683d402eb9f6b7ab6f2672a9a6cb4b19
Jungho <a01053912492@gmail.com>
2022-11-08 23:18:42 +0900

    3003

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int kingCntRequired = 1 - Integer.parseInt(st.nextToken());
+        int queenCntRequired = 1 - Integer.parseInt(st.nextToken());
+        int rookCntRequired = 2 - Integer.parseInt(st.nextToken());
+        int bishopCntRequired = 2 - Integer.parseInt(st.nextToken());
+        int knightCntRequired = 2 - Integer.parseInt(st.nextToken());
+        int pawnCntRequired = 8 - Integer.parseInt(st.nextToken());
+
+        bw.write(String.valueOf(kingCntRequired) + " "
+                + String.valueOf(queenCntRequired) + " "
+                + String.valueOf(rookCntRequired) + " "
+                + String.valueOf(bishopCntRequired) + " "
+                + String.valueOf(knightCntRequired) + " "
+                + String.valueOf(pawnCntRequired));
+
+        bw.flush();
+        bw.close();
     }
 }
