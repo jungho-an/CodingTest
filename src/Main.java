03ce79c7c670b76453f58f2c3ea3a6cbeaa26697
Jungho <a01053912492@gmail.com>
2022-12-18 00:22:53 +0900

    1185

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,53 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
+        int length, N;
+        HashSet<String>[] wordArr;
+        ArrayList<String> tempList;
 
-        int a, b;
+        st = new StringTokenizer(br.readLine());
 
-        while(true) {
+
+        N = Integer.parseInt(st.nextToken());
+
+        wordArr = new HashSet [51];
+
+
+        for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
+            String word = st.nextToken();
+            length = word.length();
 
-            if(a == 0 && b == 0) {
-                break;
+            if(wordArr[length] == null) {
+                wordArr[length] = new HashSet<String>();
             }
+            wordArr[length].add(word);
+        }
 
-            if(a > b) {
-                bw.write("Yes");
-                bw.newLine();
-            }else {
-                bw.write("No");
-                bw.newLine();
+        for(int i=1; i<=50; i++) {   //단어 길이 1 ~ 50까지 반복
+            if(wordArr[i] != null) {
+                tempList = new ArrayList<String>(wordArr[i]);
+                Collections.sort(tempList);
+
+                for(int j=0; j<tempList.size(); j++) {
+                    bw.write(tempList.get(j));
+                    bw.newLine();
+                }
             }
         }
 
         bw.flush();
         bw.close();
     }
+
+
 }
