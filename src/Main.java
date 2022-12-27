4b6abd6e7eac034a1a883e29ccd3d6dd64feaa82
Jungho <a01053912492@gmail.com>
2022-12-27 21:23:21 +0900

    2292

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,40 +7,19 @@ public class Main {
     static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
-        int length, N;
-        HashSet<String>[] wordArr;
-        ArrayList<String> tempList;
+        int N, res = 0, comp = 1;
 
         st = new StringTokenizer(br.readLine());
-
-
         N = Integer.parseInt(st.nextToken());
 
-        wordArr = new HashSet [51];
-
-
-        for(int i=0; i<N; i++) {
-            st = new StringTokenizer(br.readLine());
-            String word = st.nextToken();
-            length = word.length();
-
-            if(wordArr[length] == null) {
-                wordArr[length] = new HashSet<String>();
+        while(true) {
+            if(N <= comp) {
+                break;
             }
-            wordArr[length].add(word);
+            res += 1;
+            comp += 6 * res;
         }
-
-        for(int i=1; i<=50; i++) {   //단어 길이 1 ~ 50까지 반복
-            if(wordArr[i] != null) {
-                tempList = new ArrayList<String>(wordArr[i]);
-                Collections.sort(tempList);
-
-                for(int j=0; j<tempList.size(); j++) {
-                    bw.write(tempList.get(j));
-                    bw.newLine();
-                }
-            }
-        }
+        bw.write(Integer.toString(res + 1));
 
         bw.flush();
         bw.close();
