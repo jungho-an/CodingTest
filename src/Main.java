4e33fd6a9f082033e2479d966ddadb29de4c7428
Jungho <a01053912492@gmail.com>
2023-01-05 22:24:08 +0900

    1238

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -34,9 +35,44 @@ public class Main {
         }
     }
     public static void main(String[] args) throws IOException {
+        int N, M, X;
+        int [] tArr;
 
-        bw.flush();
-        bw.close();
+        ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+        List<Integer> resList = new ArrayList<>();
+
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+        tArr = new int[N];
+
+        for(int i=0; i<N; i++) {
+            graph.add(new ArrayList<Node>());
+        }
+
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int T = Integer.parseInt(st.nextToken());
+
+            graph.get(A - 1).add(new Node(B - 1, T));
+        }
+
+        int begin = 0;
+        while(true) {
+            Arrays.fill(tArr, -1);
+            for(Node node : graph.get(begin)) {
+
+            }
+
+        }
+
+
+//        bw.flush();
+//        bw.close();
     }
 
 
