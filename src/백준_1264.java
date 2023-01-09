303115d275168083c4502a84754e0c48356f9b4d
Jungho <a01053912492@gmail.com>
2023-01-10 01:21:42 +0900

    1264 - Clear

---
+++ src/백준_1264.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class 백준_1264 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> inputList = new ArrayList<>();
+
+        while(true) {
+            String tmp = br.readLine();
+            if(tmp.equals("#")) {
+                break;
+            }else {
+                inputList.add(tmp);
+            }
+        }
+
+        //////
+
+        for(String tmp : inputList) {
+            String modifiedTmp = tmp;
+            modifiedTmp = modifiedTmp.replaceAll("a", "");
+            modifiedTmp = modifiedTmp.replaceAll("e", "");
+            modifiedTmp = modifiedTmp.replaceAll("i", "");
+            modifiedTmp = modifiedTmp.replaceAll("o", "");
+            modifiedTmp = modifiedTmp.replaceAll("u", "");
+            modifiedTmp = modifiedTmp.replaceAll("A", "");
+            modifiedTmp = modifiedTmp.replaceAll("E", "");
+            modifiedTmp = modifiedTmp.replaceAll("I", "");
+            modifiedTmp = modifiedTmp.replaceAll("O", "");
+            modifiedTmp = modifiedTmp.replaceAll("U", "");
+            bw.write(Integer.toString(tmp.length() - modifiedTmp.length()));
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+
+    }
+}
