bc53619bda3e7b88179261dc2e2d24950d081d96
정호 <a01053912492@gmail.com>
2023-04-15 00:32:55 +0900

    백준_10926-Clear

---
+++ src/백준_10926.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class 백준_10926 {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        String strId = st.nextToken();
+        bw.write(strId + "??!");
+        bw.flush();
+        bw.close();
+    }
+}
