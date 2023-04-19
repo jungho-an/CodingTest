89b2021ca149abfb91f4129fb893705a12e01b33
정호 <a01053912492@gmail.com>
2023-04-19 23:48:32 +0900

    백준_18108-Clear

---
+++ src/백준_18108.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class 백준_18108 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int y, ans;
+        st = new StringTokenizer(br.readLine());
+        y = Integer.parseInt(st.nextToken());
+        ans = y - 543;
+        bw.write(Integer.toString(ans));
+        bw.flush();
+        bw.close();
+    }
+}
